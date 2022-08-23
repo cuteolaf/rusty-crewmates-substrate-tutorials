be52c81437b88d76330df2f0bcb918726af1d812
cuteolaf <oliverlim818@gmail.com>
2022-08-22 20:38:23 -0700

    solution: ex03

--- exercises/ex03-nft/nft/src/lib.rs
+++ exercises/ex03-nft/nft/src/lib.rs
@@ -94,11 +94,72 @@ pub mod pallet {
 			metadata: BoundedVec<u8, T::MaxLength>,
 			supply: u128,
 		) -> DispatchResult {
+			// Ensure that the extrinsic is signed
+			let owner = ensure_signed(origin)?;
+
+			ensure!(supply > 0, Error::<T>::NoSupply);
+
+			let asset_id = Self::nonce();
+			Nonce::<T>::set(asset_id + 1);
+
+			let details = UniqueAssetDetails::new(owner.clone(), metadata, supply);
+
+			UniqueAsset::<T>::insert(asset_id, details);
+
+			Account::<T>::insert(asset_id, owner.clone(), supply);
+
+			Self::deposit_event(Event::<T>::Created {
+				creator: owner,
+				asset_id,
+			});
+
 			Ok(())
 		}
 
 		#[pallet::weight(0)]
 		pub fn burn(origin: OriginFor<T>, asset_id: UniqueAssetId, amount: u128) -> DispatchResult {
+			// Ensure the extrinsic is signed
+			let owner = ensure_signed(origin)?;
+
+			// Ensure the asset id exists
+			ensure!(
+				UniqueAsset::<T>::contains_key(asset_id),
+				Error::<T>::UnknownAssetId
+			);
+
+			ensure!(
+				Self::account(asset_id, owner.clone()) > 0,
+				Error::<T>::NotOwned
+			);
+
+			let mut burnt_amount = 0;
+			let mut total_supply = 0;
+
+			// Mutate the account balance
+
+			Account::<T>::mutate(asset_id, owner.clone(), |balance| -> DispatchResult {
+				let old_balance = *balance;
+				*balance = old_balance.saturating_sub(amount);
+				burnt_amount = old_balance - *balance;
+
+				Ok(())
+			})?;
+
+			UniqueAsset::<T>::mutate(asset_id, |details| -> DispatchResult {
+				let details = details.as_mut().ok_or(Error::<T>::UnknownAssetId)?;
+
+				details.supply = details.supply.saturating_sub(burnt_amount);
+				total_supply = details.supply;
+
+				Ok(())
+			})?;
+
+			Self::deposit_event(Event::<T>::Burned {
+				asset_id,
+				owner,
+				total_supply,
+			});
+
 			Ok(())
 		}
 
@@ -109,6 +170,36 @@ pub mod pallet {
 			amount: u128,
 			to: T::AccountId,
 		) -> DispatchResult {
+			let from = ensure_signed(origin)?;
+
+			ensure!(
+				UniqueAsset::<T>::contains_key(asset_id) == true,
+				Error::<T>::UnknownAssetId
+			);
+			ensure!(
+				Self::account(asset_id, from.clone()) > 0,
+				Error::<T>::NotOwned
+			);
+
+			let mut transferred = 0;
+
+			Account::<T>::mutate(asset_id, from.clone(), |balance| {
+				let old_balance = *balance;
+				*balance = old_balance.saturating_sub(amount);
+				transferred = old_balance - *balance;
+			});
+
+			Account::<T>::mutate(asset_id, to.clone(), |balance| {
+				*balance += transferred;
+			});
+
+			Self::deposit_event(Event::<T>::Transferred {
+				asset_id,
+				from,
+				to,
+				amount: transferred,
+			});
+
 			Ok(())
 		}
 	}
